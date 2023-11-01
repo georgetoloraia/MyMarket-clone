340bc9fee855f3190a6d563ff957d4746eddf17b
georgetoloraia <gtoloraia19@gmail.com>
2023-11-01 23:50:05 +0400

    add - [x] CORS_ALLOWED_ORIGINS = ['*']

--- mymarket/mymarket/settings.py
+++ mymarket/mymarket/settings.py
@@ -150,10 +150,8 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    # You can add other domains here
-]
+CORS_ALLOWED_ORIGINS = ["*"]
+
 
 
 SWAGGER_SETTINGS = {
